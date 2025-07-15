9d3e8f0bf7cf2c8c0ae3680f78889a2823747faf
Chong Wei Hon <c.weihon@gmail.com>
2025-07-15 17:17:41 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ import { R2Explorer } from "r2-explorer";
 export default R2Explorer({
   // Set to false to allow users to upload files
   readonly: false,
-
+  asicAuth: [
+    {
+      username: 'cloud',
+      password: 'cloudamind123'
+    }]
   // Learn more how to secure your R2 Explorer instance:
   // https://r2explorer.com/getting-started/security/
   // cfAccessTeamName: "my-team-name",
